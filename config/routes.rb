0b6139971de47572dbfc283f5b5eb975989ebb54
papajo <joshipv2@gmail.com>
2015-04-05 22:31:34 -0400

    Added change task custom action

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
-  resources :tasks
+  resources :tasks do
+    member do
+      put :change
+    end
+  end
 
   devise_for :users
   root 'pages#home'
 
   get 'about' => "pages#about"
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
